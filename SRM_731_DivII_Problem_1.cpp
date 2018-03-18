b9284175b557fc293919c547f5af20aca4e2b91f
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-03-18 13:52:19 -0400

    Create SRM_731_DivII_Problem_1.cpp

---
+++ SRM_731_DivII_Problem_1.cpp
@@ -0,0 +1,27 @@
+// code_report Solution
+// 
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string getmin (string t) 
+{
+	int n = t.size ();
+	string s (n, 'z');
+
+	const vector<int> primes { 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47 };
+
+	for (int i = 0; i < n; ++i) {
+		for (int prime : primes) {
+			if (prime >= n) break;
+			string temp;
+			for (int j = 0; j < n; ++j) temp += t[(i + prime * j) % n];
+			s = min (s, temp);
+		}
+	}
+
+	return s;
+}
