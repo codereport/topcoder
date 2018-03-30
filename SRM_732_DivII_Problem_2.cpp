7e7547c30b9f468456afe77f8f82ba86c3d55d78
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-03-30 11:47:41 -0400

    Update SRM_732_DivII_Problem_2.cpp

--- SRM_732_DivII_Problem_2.cpp
+++ SRM_732_DivII_Problem_2.cpp
@@ -55,3 +55,48 @@ public:
 	}
 
 };
+
+// un-rolled out, more STL solution
+
+class CubeStackingGame {
+public:
+
+	int res = 0, N;
+	vector<int> e, f, g, h;
+	vector<unordered_set<int>> s; // stacks
+
+	void modify_stacks_then_dfs (int i, const vector<int>& c) {
+		bool perform_dfs = true;
+		for (int j = 0; j < 4; j++) if (s[j].count (c[j])) perform_dfs = false;
+
+		if (perform_dfs) {
+			vector<unordered_set<int>::iterator> to_erase (4);
+			for (int j = 0; j < 4; ++j) to_erase[j] = s[j].insert (c[j]).first;
+			dfs (i + 1);
+			for (int j = 0; j < 4; j++) s[j].erase (to_erase[j]);
+		}
+	}
+
+	void dfs (int i) {
+		res = max (res, i);
+		if (i == N) return;
+
+		vector<int> c = { e[i], f[i], g[i], h[i] }; // colors
+
+		for (int j = 0; j < 8; j++) {
+			if (j == 4) reverse (c.begin (), c.end ());
+			modify_stacks_then_dfs (i, c);
+			rotate (c.begin (), c.begin () + 1, c.end ());
+		}
+	}
+
+	int MaximumValue (int n, vector <int> c1, vector <int> c2, vector <int> c3, vector <int> c4) {
+		e = c1, f = c2, g = c3, h = c4, N = n;
+		s.resize (4);
+
+		dfs (0);
+
+		return res;
+	}
+
+};
