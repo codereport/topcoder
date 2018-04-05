c05f910f899e0a2bec71911476833dadd339722b
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-04-05 11:32:32 -0400

    Update SRM_732_DivII_Problem_3.cpp

--- SRM_732_DivII_Problem_3.cpp
+++ SRM_732_DivII_Problem_3.cpp
@@ -14,21 +14,23 @@ static const vector<int> dy = { 0 , 1 , 0 , -1 };
 
 class TheFlippingGame2 {
 public:
-	int explore (int H, int W, vector<string> grid) 
+
+	int explore (int H, int W, const vector<string>& grid) 
 	{
 		int min_flips = numeric_limits<int>::max ();
-		
+		vector<vector<int>> explored (H, vector<int> (W, -1));
+
 		for (int sy = 0; sy < H; ++sy) {
 			for (int sx = 0; sx < W; ++sx) 
 			{
-				vector<vector<int>> done (H, vector<int> (W, -1));
+				for (auto& row : explored) fill (row.begin (), row.end (), -1);
 				priority_queue<pair<int,pair<int,int>>> q;
 				int max_flips = 0;
 
-				done[sy][sx] = 0;
+				explored[sy][sx] = 0;
 				q.push ({ 0 , { sy, sx } });
 
-				while (q.size ()) 
+				while (!q.empty ()) 
 				{
 					int cf = -q.top ().first;
 					int cy =  q.top ().second.first;
@@ -43,10 +45,10 @@ public:
 						int nx = cx + dx[i];
 						if (ny < 0 || ny >= H || nx < 0 || nx >= W) continue;
 						
-						if (done[ny][nx] == -1) 
+						if (explored[ny][nx] == -1) 
 						{
-							done[ny][nx] = cf + (grid[cy][cx] != grid[ny][nx]);
-							q.push ({ -done[ny][nx], { ny, nx } });
+							explored[ny][nx] = cf + (grid[cy][cx] != grid[ny][nx]);
+							q.push ({ -explored[ny][nx], { ny, nx } });
 						}
 					}
 				}
@@ -66,11 +68,8 @@ public:
 		for (auto color : colors) 
 		{
 			vector<string> filled_grid = x;
+			for (auto& row : filled_grid) replace (row.begin (), row.end (), 'e', color);
 			
-			for (int i = 0; i < n; ++i)
-				for (int j = 0; j < m; ++j)
-					if (x[i][j] == 'e') filled_grid[i][j] = color;
-
 			min_flips = min (min_flips, explore (n, m, filled_grid));
 		}
 
