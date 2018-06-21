ec190f71229f521bcb314283ef1171f84af00c48
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-06-21 08:39:33 -0400

    Update SRM_TCO18_Round_2B_P1.cpp

--- SRM_TCO18_Round_2B_P1.cpp
+++ SRM_TCO18_Round_2B_P1.cpp
@@ -20,9 +20,10 @@ vector<double> findBest (const vector<int>& a)
       for (auto i = start; i != b.end (); ++i) {
          sum  += *i;
          avg_i = sum / (distance (start, i) + 1);
-         if (avg_i < avg) avg = avg_i, end = i + 1;
+         if (avg_i < avg) avg = avg_i, end = i;
       }
 
+      ++end;
       fill (start, end, avg);
       start = end;
    }
