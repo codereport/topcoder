5711e04c2398ffb431353f5536c58260a5fa1cf5
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-11-30 00:55:27 -0800

    Create SRM_742_DivII_Problem_2.cpp

---
+++ SRM_742_DivII_Problem_2.cpp
@@ -0,0 +1,65 @@
+// code_report Solution
+// 
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <iterator>
+#include <numeric>
+
+#define FORI(s,n) for(int i = s; i < n; i++)
+#define FORJ(s,n) for(int j = s; j < n; j++)
+
+using namespace std;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+class chess_board {
+   vvi g;
+   vi dx = { 0,  0, 1, -1, 1, 1, -1, -1 };
+   vi dy = { 1, -1, 0,  0, 1, -1, 1, -1 };
+
+   bool is_valid(int x, int y) {
+      return x >= 0 && x < 50 && y >= 0 && y < 50;
+   }
+
+public:
+
+   chess_board() : g(50, vi(50, 0)) {}
+    
+   void place_queen(int x, int y) {
+      FORI(0, 8) {
+         int r = x, c = y;
+         while (is_valid(r, c)) 
+            g[r][c] = 1, r += dx[i], c += dy[i];
+      }
+   }
+
+   pair<int,int> find_and_place_next_queen() {
+      FORI(0, 50) {
+         FORJ(0, 50) {
+            if (g[i][j]) continue;
+            place_queen(i, j);
+            return make_pair(i, j);
+         }
+      }
+   }
+};
+
+class SixteenQueens {
+public:
+   vi addQueens(vi row, vi col, int add) {
+      chess_board cb;
+      for (int i = 0; i < row.size(); ++i) 
+         cb.place_queen(row[i], col[i]);
+      vi ans;
+      while (add--) {
+         pair<int, int> p = cb.find_and_place_next_queen();
+         ans.push_back(p.first);
+         ans.push_back(p.second);
+      }
+      return ans;
+   }
+};
