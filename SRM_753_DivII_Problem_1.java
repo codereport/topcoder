49257abc14a4deeaf2d70a1ab06b92952a7136ba
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-03-22 19:51:33 -0700

    Create SRM_753_DivII_Problem_1.java

---
+++ SRM_753_DivII_Problem_1.java
@@ -0,0 +1,29 @@
+// code_report Solution
+// Video Link:
+// Problem Link: https://arena.topcoder.com/#/u/practiceCode/17449/85533/15355/2/332231
+
+// Solution 1
+
+import java.lang.String;
+
+public class KerimJavati {
+    public int howLong(String text) {
+        int ans = 0;
+        for (char c : text.toCharArray()) {
+            ans += 2 * (c - 'a') + 1;
+        }
+        return ans;
+    }
+}
+
+// Solution 2
+
+import java.lang.String;
+
+public class KerimJavati {
+    public int howLong(String text) {
+        return text
+            	  .chars()
+            	  .reduce(0, (a, b) -> a + 2 * (b - 'a') + 1);
+    }
+}
