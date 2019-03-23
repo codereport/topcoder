ab5f38a800219621db9c45d491a9e6570669494a
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-03-22 19:35:38 -0700

    Create SRM_753_DivII_Problem_1.cpp

---
+++ SRM_753_DivII_Problem_1.cpp
@@ -0,0 +1,16 @@
+// code_report Solution
+// Video Link:
+// Problem Link: https://arena.topcoder.com/#/u/practiceCode/17449/85533/15355/2/332231
+
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+class KerimJavati {
+  public:
+  int howLong(string text) {
+    return accumulate(text.begin(), text.end(), 0, 
+      [](int a, char b) { return a + 2 * (b - 'a') + 1; });
+  }
+};
